73c0abef841e5675eed9fa7a2033790cc3d425d1
Axel Ramos <axl611@gmail.com>
2026-03-23 21:20:26 -0600

    Project setup and foundations

---
+++ src/pages/components/BaseComponent.ts
@@ -0,0 +1,31 @@
+import { Page, Locator } from '@playwright/test';
+
+export class BaseComponent {
+    
+    constructor(protected page: Page) {
+
+    }
+
+    protected async waitForElement(locator: Locator): Promise<void> {
+        await locator.waitFor({ state: 'visible' });
+
+    }
+
+    protected async clickElement(locator: Locator): Promise<void> {
+        await this.waitForElement(locator);
+        await locator.click();
+
+    }
+
+    protected async fillField(locator: Locator, value: string): Promise<void> {
+        await this.waitForElement(locator);
+        await locator.fill(value);
+
+    }
+
+    protected async getText(locator: Locator): Promise<string> {
+        await this.waitForElement(locator);
+        return await locator.innerText();
+        
+    }
+}
